7d21dffe7461a6ead51141dbdaf7dd0e10038ca2
WooMH <woominho4756@gmail.com>
2024-09-24 16:37:11 +0900

    주석 추가 및 변수 이름 변경

--- Assets/02. Scripts/Enemy.cs
+++ Assets/02. Scripts/Enemy.cs
@@ -5,31 +5,27 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     public float speed;
-    public Rigidbody2D target;
+    public Rigidbody2D player;
 
     bool isLive = true;
 
-    Rigidbody2D rigid;
-    SpriteRenderer spriter;
+    Rigidbody2D enemy;
     void Awake()
     {
-        rigid = GetComponent<Rigidbody2D>();
-        spriter = GetComponent<SpriteRenderer>();
+        enemy = GetComponent<Rigidbody2D>();
     }
 
     private void FixedUpdate()
     {
+        // 적이 죽은 상태라면 작동 X
         if (!isLive)
             return;
-
-        Vector2 dirVec = target.position - rigid.position;
+        // 목표의 위치와 현재 적의 위치를 빼서 방향 벡터 설정
+        Vector2 dirVec = player.position - enemy.position;
+        // dirVec을 normalized하는 이유는 모든 방향의 길이를 정규화해놓아야 이동 속도가 같아지기 때문이다.
         Vector2 nextVec = dirVec.normalized * speed * Time.fixedDeltaTime;
-        rigid.MovePosition(rigid.position + nextVec);
-        rigid.velocity = Vector2.zero;
-    }
-
-    private void LateUpdate()
-    {
-        spriter.flipX = target.position.x < rigid.position.x;
+        // 적을 nextVec만큼 이동
+        enemy.MovePosition(enemy.position + nextVec);
+        enemy.velocity = Vector2.zero;
     }
 }
