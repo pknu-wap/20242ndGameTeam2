a22916a0162c2b03ec2ec8885570ea42eb9389aa
minjun <57467950+minjun-ludigames2019@users.noreply.github.com>
2024-10-03 22:36:57 +0900

    스크립트 버튼 수정
    PlayerMovement 스크립트 버튼 부분 삭제 및 유니티 인스펙터상에서 구르기 버튼 누를시 onclick()메서드 직접 호출하게 수정함

--- Assets/02.Scripts/PlayerMovement.cs
+++ Assets/02.Scripts/PlayerMovement.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    /*접근 지정자 or 접근 제한자
-    public , private, protected 공부해오기
-
+    /*
     유니티 물리 rigidbody2D
     Body Type에 다이나믹 키네마틱 스테틱 차이 알아오기 */
-    public float moveSpeed = 5f;
+
+    [SerializeField]
+    private float moveSpeed = 5f;
     public float rollSpeed = 10f;    // 구르기 시 속도
     public float rollDuration = 0.5f; // 구르기 지속 시간
     private bool isRolling = false;
@@ -17,14 +17,10 @@ public class PlayerMovement : MonoBehaviour
     private Vector2 moveDirection;
 
     public Joystick joystick;        // Joystick 연결
-    public GameObject rollButton;    // 구르기 버튼 연결
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-
-        // 구르기 버튼 클릭 시 Roll() 메서드 호출
-        rollButton.GetComponent<UnityEngine.UI.Button>().onClick.AddListener(Roll);
     }
 
     void Update()
@@ -53,7 +49,7 @@ public class PlayerMovement : MonoBehaviour
     }
 
     // 구르기 동작 실행
-    void Roll()
+    public void Roll()
     {
         if (!isRolling)
         {
@@ -61,7 +57,7 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    IEnumerator PerformRoll()
+    IEnumerator PerformRoll() //코루틴
     {
         isRolling = true;
         Vector2 rollDirection = moveDirection;
