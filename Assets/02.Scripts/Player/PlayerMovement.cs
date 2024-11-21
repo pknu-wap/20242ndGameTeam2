747a964c75f062ec1ad39cf82939b0bbd3bbc5a3
skypukyong <didani1234@naver.com>
2024-11-21 23:29:20 +0900

    구르기시 적 넘어갈 수 있게(애매하게 완성)

--- Assets/02.Scripts/Player/PlayerMovement.cs
+++ Assets/02.Scripts/Player/PlayerMovement.cs
@@ -15,9 +15,21 @@ public class PlayerMovement : MonoBehaviour
 
     public Joystick joystick;        // Joystick 연결
 
+    private Collider2D[] enemyColliders; // 적의 콜라이더들 저장
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");//enemy 태그를 가진 적들을 찾고 배열에 포함(비활성화된 오브젝트에서는 못찾음)
+        //이 배열을 초기화할 적당한 타이밍이 필요, 방 넘어갈때마다 갱신하거나 몬스터 소환 될 때마다 갱신하거나
+        enemyColliders = new Collider2D[enemies.Length]; // Collider2D 배열 크기 설정
+
+        // 각 적의 Collider2D 컴포넌트를 enemyColliders 배열에 저장
+        for (int i = 0; i < enemies.Length; i++)
+        {
+            enemyColliders[i] = enemies[i].GetComponent<Collider2D>();
+        }
     }
 
     void Update()
@@ -54,7 +66,7 @@ public class PlayerMovement : MonoBehaviour
     public void Roll()
     {
         if (!isRolling)
-        {   
+        {
             StartCoroutine(PerformRoll());
         }
     }
@@ -63,6 +75,15 @@ public class PlayerMovement : MonoBehaviour
     {
         isRolling = true;
         TutorialManager.isInvincible = true;
+
+        
+
+        // 플레이어와 적 간의 충돌을 무시
+        foreach (var enemy in enemyColliders)
+        {
+            Physics2D.IgnoreCollision(GetComponent<Collider2D>(), enemy, true);
+        }
+
         // 구르기 속도를 적용하여 움직임 시작
         float elapsedTime = 0f;
         while (elapsedTime < rollDuration)
@@ -71,8 +92,14 @@ public class PlayerMovement : MonoBehaviour
             elapsedTime += Time.deltaTime;
             yield return null; // 다음 프레임까지 대기
         }
+
+        // 구르기 후, 적과의 충돌을 다시 활성화
+        foreach (var enemy in enemyColliders)
+        {
+            Physics2D.IgnoreCollision(GetComponent<Collider2D>(), enemy, false);
+        }
+
         TutorialManager.isInvincible = false;
         isRolling = false;
-
     }
 }
