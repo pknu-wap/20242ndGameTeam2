a0eea37239f6fe4e02ef8f198f548ad6d459e429
WooMH <woominho4756@gmail.com>
2024-11-27 02:54:11 +0900

    레벨업 시스템 구현 완료

--- Assets/02.Scripts/Enemy/CloseRangeEnemy.cs
+++ Assets/02.Scripts/Enemy/CloseRangeEnemy.cs
@@ -20,7 +20,7 @@ public class CloseRangeEnemy : BaseEnemy
         base.Awake();
         enemy = GetComponent<Rigidbody2D>();
         damageMultiplier = 1.0f;
-        expOnDeath = 5000;
+        expOnDeath = 1000000;
     }
 
     private void FixedUpdate()
