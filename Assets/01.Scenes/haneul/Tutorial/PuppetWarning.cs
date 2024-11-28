b5c8a7abad00bff002c5f992ca56e67844b20302
skypukyong <didani1234@naver.com>
2024-11-28 21:32:04 +0900

    퍼펫 기능 구현

--- Assets/01.Scenes/haneul/Tutorial/PuppetWarning.cs
+++ Assets/01.Scenes/haneul/Tutorial/PuppetWarning.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PuppetWarning : MonoBehaviour
 {
+    public GameObject wall;
     void OnTriggerEnter2D(Collider2D col)
     {
         if (col.gameObject.CompareTag("Player"))
@@ -13,7 +14,7 @@ public class PuppetWarning : MonoBehaviour
 
             // 부모 객체에서 PuppetMarkMove 컴포넌트를 가져와 ChangeSprite 호출
             parent.GetComponent<PuppetMarkMove>().ChangeSprite();
-
+            wall.SetActive(true);
             // 현재 게임 오브젝트 파괴
             Destroy(gameObject);
         }
