10eeeec23600f247dac3cd7c8c6c4786935f3f3b
skypukyong <didani1234@naver.com>
2024-11-29 09:51:31 +0900

    임시저장

---
+++ Assets/HandDamage.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandDamage : MonoBehaviour
+{
+    private bool hasCollided = false; // 충돌 여부 체크
+
+    void Awake()
+    {
+        StartCoroutine(IcicleInstant());
+    }
+
+    private IEnumerator IcicleInstant()
+    {
+        yield return new WaitForSeconds(2.35f);
+
+        Destroy(gameObject);
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // 플레이어와 충돌했을 때
+        if (other.CompareTag("Player"))
+        {
+            hasCollided = true; // 충돌 처리 상태로 설정
+        }
+
+        else
+        {
+            hasCollided = false;
+        }
+    }
+
+    // 애니메이션 이벤트에서 호출할 함수
+    public void DealDamageIfCollided()
+    {
+
+        if (hasCollided)
+        {
+            Debug.Log("으악!");
+            GameManager.Instance.TakeDamageToPlayer(1, "손아귀");
+        }
+    }
+}
